14938deb96e9a9553668af95912c25b646b11985
EthanGoh1008 <S10258606@connect.np.edu.sg>
2023-12-02 15:02:07 +0800

    Addition of buttons to show diffferent images

--- jvsassgmt1.js
+++ jvsassgmt1.js
@@ -3,3 +3,27 @@ for (let i = 1; i <= 16; i++) {
     window.location.href = "index" + i + ".html";
   };
 }
+
+function showAll() {
+  showImages("thumbnailsalltn");
+}
+
+function showPopular() {
+  hideAllImages();
+  showImages("populartn");
+}
+
+function showRecent() {
+  hideAllImages();
+  showImages("recenttn");
+}
+
+function hideAllImages() {
+  const images = document.querySelectorAll(".thumbnailsall img");
+  images.forEach((img) => (img.style.display = "none"));
+}
+
+function showImages(className) {
+  const imagesToShow = document.querySelectorAll(`.${className}`);
+  imagesToShow.forEach((img) => (img.style.display = "block"));
+}
